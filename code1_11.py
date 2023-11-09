30fe69fd9fdc75333b457ac7e69e9bb4f10851f3
ktkt0305 <ktktsyokugyoukunrn@gmail.com>
2023-11-09 10:54:23 +0900

    First Commit

---
+++ code1_11.py
@@ -0,0 +1,9 @@
+age = 24
+print('浅木先輩の年齢は・・・')
+print(age)
+age = age + 1
+print('来年は・・・')
+print(age)
+age = age + 1
+print('再来年は・・・')
+print(age)
---
+++ rensyuu1_3.py
@@ -0,0 +1,4 @@
+h = int(input('身長(cm)を入力 >>')) /100
+w = float(input('体重(kg)を入力 >>'))
+bmi = w/h/h
+print('あなたのBMIは{}です。'.format(bmi))
